460265ad30dd3a46daf05cdf02d3a5ee50f89ee3
Sergey <paulfrere91@gmail.ru>
2020-10-14 21:00:24 +0700

    Начало проекта

---
+++ desktop/src/ru/geekbrains/zsa/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.zsa.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.zsa.game.SpaceBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceBattle(), config);
+	}
+}
