a4b63f55b4f805e55414da8bf60d838538f5cb6b
Sergey <paulfrere91@gmail.ru>
2020-10-14 21:06:35 +0700

    HomeWork_1

--- core/src/ru/geekbrains/zsa/game/SpaceBattle.java
+++ core/src/ru/geekbrains/zsa/game/SpaceBattle.java
@@ -18,11 +18,13 @@ public class SpaceBattle extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		batch = new SpriteBatch();
+		img = new Texture("background.jpg");
 	}
 	
 	@Override
